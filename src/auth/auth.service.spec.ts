46d9fe0eabef3a9b526d12bc153ae8832073b7d1
Emmanuel Okpunor <okpunorrex@gmail.com>
2024-02-19 12:49:39 +0100

    chore: authentication testing

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,6 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { ModuleMocker, MockFunctionMetadata } from 'jest-mock';
+import { resolve } from 'path';
+import { TokenPayload } from 'google-auth-library';
+import { registerStud } from './stud/auth.stud';
 
 const moduleMocker = new ModuleMocker(global);
 
@@ -14,7 +17,11 @@ describe('AuthService', () => {
       .useMocker((token) => {
         const result = { access_token: 'some_token' };
         if (token === AuthService) {
-          return { register: jest.fn().mockResolvedValue(result) };
+          return {
+            register: jest.fn().mockResolvedValue(result),
+            login: jest.fn().mockResolvedValue(result),
+            socialLogin: jest.fn().mockResolvedValue(result),
+          };
         }
         if (typeof token === 'function') {
           const mockMetadata = moduleMocker.getMetadata(
@@ -31,5 +38,42 @@ describe('AuthService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+  describe('register', () => {
+    it('should return access token on register', async () => {
+      const result = { access_token: 'some_token' };
+
+      jest.spyOn(service, 'register').mockResolvedValue(result);
+
+      expect(await service.register(registerStud())).toEqual(result);
+    });
+  });
+  describe('login', () => {
+    it('should return access token on login', async () => {
+      const result = { access_token: 'some_token' };
+      const { email, password } = registerStud();
+      jest.spyOn(service, 'login').mockResolvedValue(result);
+
+      expect(
+        await service.login({
+          email,
+          password,
+        }),
+      ).toEqual(result);
+    });
+  });
+  describe('socialLogin', () => {
+    it('should return access token on register', async () => {
+      const result = { access_token: 'some_token' };
+      jest.spyOn(service, 'socialLogin').mockResolvedValue(result);
+
+      expect(
+        await service.socialLogin({
+          provider: 'google',
+          token: 'google token',
+          fcmToken: 'fcm token',
+        }),
+      ).toEqual(result);
+    });
   });
 });
