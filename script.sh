31b1206b3879efed753d71f298cf4717b2e1daae
Emmanuel Okpunor <okpunorrex@gmail.com>
2024-02-20 01:33:12 +0100

    chore: finished up all set up

--- script.sh
+++
@@ -1,5 +0,0 @@
-docker buildx build --platform=linux/amd64 -f Dockerfile.prod -t okp980/wemeet-backend .
-
-docker push okp980/wemeet-backend   
-
-docker run -p 3000:3000 --env-file ./.env.production --rm --name wemeets okp980/wemeet-backend
--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,25 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
+  app.setGlobalPrefix('api/v1');
+
   const config = new DocumentBuilder()
     .addBearerAuth()
     .setTitle('WeMeet')
     .setDescription(
       'Rest API for WeMeet Chat application. it contains CRUD functionalities for friend requests, user onboarding and authentication.',
     )
-    .setVersion('1.0')
+    .addServer('https://wemeet-app.onrender.com/')
+    .addServer('http://localhost:3000/')
+    .setVersion('1.0.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document, {
+  SwaggerModule.setup('api/v1/docs', app, document, {
     customSiteTitle: 'App API',
   });
   app.useGlobalPipes(new ValidationPipe());
